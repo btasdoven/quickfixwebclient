d1d6c0762721e07666f827fd125e1fcca85055f6
Batuhan Tasdoven <btasdoven@gmail.com>
2017-01-27 01:50:07 -0800

    second commit

---
+++ acceptor.go
@@ -0,0 +1,229 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"path"
+
+	"github.com/quickfixgo/quickfix"
+	"github.com/quickfixgo/quickfix/enum"
+	"github.com/quickfixgo/quickfix/field"
+	"github.com/quickfixgo/quickfix/tag"
+	"github.com/shopspring/decimal"
+
+	"github.com/FlashBoys/go-finance"
+
+	fix42nos "github.com/quickfixgo/quickfix/fix42/newordersingle"
+	fix42er "github.com/quickfixgo/quickfix/fix42/executionreport"
+    fix42mdr "github.com/quickfixgo/quickfix/fix42/marketdatarequest"
+    fix42md  "github.com/quickfixgo/quickfix/fix42/marketdatasnapshotfullrefresh"
+
+	"os"
+	"os/signal"
+	"strconv"
+)
+
+type executor struct {
+	orderID int
+	execID  int
+	*quickfix.MessageRouter
+}
+
+func newExecutor() *executor {
+	e := &executor{MessageRouter: quickfix.NewMessageRouter()}
+	e.AddRoute(fix42nos.Route(e.OnFIX42NewOrderSingle))
+	e.AddRoute(fix42mdr.Route(e.OnFIX42MarketDataRequest))
+
+	return e
+}
+
+func (e *executor) genOrderID() field.OrderIDField {
+	e.orderID++
+	return field.NewOrderID(strconv.Itoa(e.orderID))
+}
+
+func (e *executor) genExecID() field.ExecIDField {
+	e.execID++
+	return field.NewExecID(strconv.Itoa(e.execID))
+}
+
+//quickfix.Application interface
+func (e executor) OnCreate(sessionID quickfix.SessionID)                           { return }
+func (e executor) OnLogon(sessionID quickfix.SessionID)                            { return }
+func (e executor) OnLogout(sessionID quickfix.SessionID)                           { return }
+func (e executor) ToAdmin(msg *quickfix.Message, sessionID quickfix.SessionID)     { return }
+func (e executor) ToApp(msg *quickfix.Message, sessionID quickfix.SessionID) error { return nil }
+func (e executor) FromAdmin(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
+	return nil
+}
+
+//Use Message Cracker on Incoming Application Messages
+func (e *executor) FromApp(msg *quickfix.Message, sessionID quickfix.SessionID) (reject quickfix.MessageRejectError) {
+	fmt.Printf("Received %v\n", msg)
+	return e.Route(msg, sessionID)
+}
+
+func (e *executor) OnFIX42MarketDataRequest(msg fix42mdr.MarketDataRequest, sessionID quickfix.SessionID) (reject quickfix.MessageRejectError) {
+    fmt.Printf("[SERVER] - MDR: %+v\n", msg.Message)
+
+	mdReqID, _ := msg.GetMDReqID()
+
+    noRelatedSym, _ := msg.GetNoRelatedSym()
+
+	len := noRelatedSym.Len()
+
+	if len <= 0 {
+		return
+	}
+
+    symbol, _ := noRelatedSym.Get(0).GetSymbol()
+    fmt.Printf("\tSymbol: %+v\n", symbol)
+
+	stock, _ := finance.GetQuote(symbol)
+
+    noMDEntryTypes, _ := msg.GetNoMDEntryTypes()
+	noMDEntries := fix42md.NewNoMDEntriesRepeatingGroup()
+
+	for i := 0; i < noMDEntryTypes.Len(); i++ {
+		noMDEntryType, _ := noMDEntryTypes.Get(i).GetMDEntryType()
+		noMDEntries.Add().SetMDEntryType(noMDEntryType)
+
+		switch noMDEntryType {
+		case enum.MDEntryType_BID:
+			noMDEntries.Get(i).SetMDEntryPx(decimal.Decimal(stock.Bid), 5)
+			noMDEntries.Get(i).SetMDEntrySize(decimal.NewFromFloat(5), 5)
+		case enum.MDEntryType_OFFER:
+			noMDEntries.Get(i).SetMDEntryPx(decimal.Decimal(stock.Ask), 5)
+			noMDEntries.Get(i).SetMDEntrySize(decimal.NewFromFloat(6), 5)
+		case enum.MDEntryType_TRADE:
+			noMDEntries.Get(i).SetMDEntryPx(decimal.Decimal(stock.LastTradePrice), 5)
+			noMDEntries.Get(i).SetMDEntrySize(decimal.NewFromFloat(13), 5)
+		default:
+			noMDEntries.Get(i).SetMDEntryPx(decimal.NewFromFloat(199), 5)
+		}
+
+		noMDEntries.Get(i).SetMDEntryPx(decimal.NewFromFloat(float64(10*(i+2))), 5)
+
+		if noMDEntryType == enum.MDEntryType_TRADE ||
+			noMDEntryType == enum.MDEntryType_OFFER ||
+			noMDEntryType == enum.MDEntryType_BID {
+		}
+	}
+
+	md := fix42md.New(field.NewSymbol(symbol))
+    md.SetMDReqID(mdReqID)    
+    md.SetNoMDEntries(noMDEntries)    
+
+    fmt.Printf("[SERVER] - Sending %+v", md)
+    quickfix.SendToTarget(md, sessionID)
+
+	return
+}
+
+func (e *executor) OnFIX42NewOrderSingle(msg fix42nos.NewOrderSingle, sessionID quickfix.SessionID) (err quickfix.MessageRejectError) {
+	ordType, err := msg.GetOrdType()
+	if err != nil {
+		return err
+	}
+
+	if ordType != enum.OrdType_LIMIT {
+		return quickfix.ValueIsIncorrect(tag.OrdType)
+	}
+
+	symbol, err := msg.GetSymbol()
+	if err != nil {
+		return
+	}
+
+	side, err := msg.GetSide()
+	if err != nil {
+		return
+	}
+
+	orderQty, err := msg.GetOrderQty()
+	if err != nil {
+		return
+	}
+
+	price, err := msg.GetPrice()
+	if err != nil {
+		return
+	}
+
+	clOrdID, err := msg.GetClOrdID()
+	if err != nil {
+		return
+	}
+
+	execReport := fix42er.New(
+		e.genOrderID(),
+		e.genExecID(),
+		field.NewExecTransType(enum.ExecTransType_NEW),
+		field.NewExecType(enum.ExecType_FILL),
+		field.NewOrdStatus(enum.OrdStatus_FILLED),
+		field.NewSymbol(symbol),
+		field.NewSide(side),
+		field.NewLeavesQty(decimal.Zero, 2),
+		field.NewCumQty(orderQty, 2),
+		field.NewAvgPx(price, 2),
+	)
+
+	execReport.SetClOrdID(clOrdID)
+	execReport.SetOrderQty(orderQty, 2)
+	execReport.SetLastShares(orderQty, 2)
+	execReport.SetLastPx(price, 2)
+
+	if msg.HasAccount() {
+		acct, err := msg.GetAccount()
+		if err != nil {
+			return err
+		}
+		execReport.SetAccount(acct)
+	}
+
+	quickfix.SendToTarget(execReport, sessionID)
+
+	return
+}
+
+func main() {
+	flag.Parse()
+
+	cfgFileName := path.Join("config", "acceptor.cfg")
+	if flag.NArg() > 0 {
+		cfgFileName = flag.Arg(0)
+	}
+
+	cfg, err := os.Open(cfgFileName)
+	if err != nil {
+		fmt.Printf("Error opening %v, %v\n", cfgFileName, err)
+		return
+	}
+
+	appSettings, err := quickfix.ParseSettings(cfg)
+	if err != nil {
+		fmt.Println("Error reading cfg,", err)
+		return
+	}
+
+	logFactory := quickfix.NewScreenLogFactory()
+	app := newExecutor()
+
+	acceptor, err := quickfix.NewAcceptor(app, quickfix.NewMemoryStoreFactory(), appSettings, logFactory)
+	if err != nil {
+		fmt.Printf("Unable to create Acceptor: %s\n", err)
+		return
+	}
+
+	err = acceptor.Start()
+	if err != nil {
+		fmt.Printf("Unable to start Acceptor: %s\n", err)
+		return
+	}
+
+	interrupt := make(chan os.Signal)
+	signal.Notify(interrupt, os.Interrupt, os.Kill)
+	<-interrupt
+
+	acceptor.Stop()
+}
---
+++ initiator.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path"
+    "time"
+
+	"github.com/quickfixgo/quickfix/field"
+	"github.com/quickfixgo/quickfix/enum"
+	"github.com/quickfixgo/quickfix"
+    
+    fix42md "github.com/quickfixgo/quickfix/fix42/marketdatasnapshotfullrefresh"
+	fix42mdr "github.com/quickfixgo/quickfix/fix42/marketdatarequest"
+)
+
+//Initiator implements the quickfix.Application interface
+type Initiator struct {
+    *quickfix.MessageRouter
+}
+
+//OnCreate implemented as part of Application interface
+func (e Initiator) OnCreate(sessionID quickfix.SessionID) {
+	return
+}
+
+//OnLogon implemented as part of Application interface
+func (e Initiator) OnLogon(sessionID quickfix.SessionID) {
+	return
+}
+
+//OnLogout implemented as part of Application interface
+func (e Initiator) OnLogout(sessionID quickfix.SessionID) {
+	return
+}
+
+//FromAdmin implemented as part of Application interface
+func (e Initiator) FromAdmin(msg *quickfix.Message, sessionID quickfix.SessionID) (reject quickfix.MessageRejectError) {
+	return
+}
+
+//ToAdmin implemented as part of Application interface
+func (e Initiator) ToAdmin(msg *quickfix.Message, sessionID quickfix.SessionID) {
+	return
+}
+
+//ToApp implemented as part of Application interface
+func (e Initiator) ToApp(msg *quickfix.Message, sessionID quickfix.SessionID) (err error) {
+	fmt.Printf("Sending %v\n", msg)
+	return
+}
+
+//FromApp implemented as part of Application interface. This is the callback for all Application level messages from the counter party.
+func (e Initiator) FromApp(msg *quickfix.Message, sessionID quickfix.SessionID) (reject quickfix.MessageRejectError) {
+	fmt.Printf("Received %v\n", msg)
+    e.Route(msg, sessionID)
+	return
+}
+
+func querySenderCompID() field.SenderCompIDField {
+	return field.NewSenderCompID("WEBUITEST")
+}
+
+func queryTargetCompID() field.TargetCompIDField {
+	return field.NewTargetCompID("FIXIMULATOR")
+}
+
+type header interface {
+	Set(f quickfix.FieldWriter) *quickfix.FieldMap
+}
+
+func queryHeader(h header) {
+	h.Set(querySenderCompID())
+	h.Set(queryTargetCompID())
+	//h.Set(queryTargetSubID())
+}
+
+func (e *Initiator) OnFIX42MarketData(msg fix42md.MarketDataSnapshotFullRefresh, sessionID quickfix.SessionID) (reject quickfix.MessageRejectError) {
+	symbol, _ := msg.GetSymbol()
+
+	noMDEntries, _ := msg.GetNoMDEntries()
+
+	for i := 0; i < noMDEntries.Len(); i++ {
+		entry := noMDEntries.Get(i)
+		entryType, _ := entry.GetMDEntryType();
+		entryPx, _ := entry.GetMDEntryPx()
+		entrySize, _ := entry.GetMDEntrySize()
+
+		fmt.Printf("%v: %v, %v %v\n", symbol, enum.MDEntryType(entryType), entryPx, entrySize)
+	}
+
+	return
+}
+
+func (e Initiator) QueryMarketDataRequest() error {
+	req := queryMarketDataRequest42()
+	return quickfix.Send(req)
+}
+
+func queryMarketDataRequest42() fix42mdr.MarketDataRequest {
+	request := fix42mdr.New(field.NewMDReqID("MARKETDATAID"),
+		field.NewSubscriptionRequestType(enum.SubscriptionRequestType_SNAPSHOT),
+		field.NewMarketDepth(0),
+	)
+
+	request.SetMDUpdateType(enum.MDUpdateType_FULL_REFRESH)
+
+	entryTypes := fix42mdr.NewNoMDEntryTypesRepeatingGroup()
+	entryTypes.Add().SetMDEntryType(enum.MDEntryType_BID)
+	entryTypes.Add().SetMDEntryType(enum.MDEntryType_OFFER)
+	entryTypes.Add().SetMDEntryType(enum.MDEntryType_TRADE)
+	entryTypes.Add().SetMDEntryType(enum.MDEntryType_TRADE_VOLUME)
+	request.SetNoMDEntryTypes(entryTypes)
+
+	relatedSym := fix42mdr.NewNoRelatedSymRepeatingGroup()
+	relatedSym.Add().SetSymbol("LNUX")
+	request.SetNoRelatedSym(relatedSym)
+
+	queryHeader(request.Header)
+	return request
+}
+
+func NewInitiator() (app Initiator) {
+	flag.Parse()
+
+	cfgFileName := path.Join("config", "initiator.cfg")
+	if flag.NArg() > 0 {
+		cfgFileName = flag.Arg(0)
+	}
+
+	cfg, err := os.Open(cfgFileName)
+	if err != nil {
+		fmt.Printf("Error opening %v, %v\n", cfgFileName, err)
+		return
+	}
+
+	appSettings, err := quickfix.ParseSettings(cfg)
+	if err != nil {
+		fmt.Println("Error reading cfg,", err)
+		return
+	}
+
+	app = Initiator{MessageRouter: quickfix.NewMessageRouter()}
+	app.AddRoute(fix42md.Route(app.OnFIX42MarketData))
+
+	fileLogFactory, err := quickfix.NewFileLogFactory(appSettings)
+
+	if err != nil {
+		fmt.Println("Error creating file log factory,", err)
+		return
+	}
+
+	initiator, err := quickfix.NewInitiator(app, quickfix.NewMemoryStoreFactory(), appSettings, fileLogFactory)
+	if err != nil {
+		fmt.Printf("Unable to create Initiator: %s\n", err)
+		return
+	}
+
+	initiator.Start()
+
+	for {
+		err = app.QueryMarketDataRequest()
+
+		if err != nil {
+			fmt.Printf("%v\n", err)
+		}
+
+		time.Sleep(5000 * time.Millisecond)
+	}
+
+	initiator.Stop()
+	return app
+}
